6cf99341b09b1f1c12d1fae4a49e435f131ce060
Shawn Tan <shawnhadritan@gmail.com>
2025-01-28 22:54:30 +0800

    feat Dashboard component v1

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,10 @@
+import TodoWidget from "./Widgets/TodoWidget";
+
+export default function Dashboard() {
+    return (
+        <main>
+            <h1>My Dashboard</h1>
+            <TodoWidget />
+        </main>
+    );
+}
