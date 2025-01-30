5644adc462ebe6574742c350346a664079fb990e
Shawn Tan <shawnhadritan@gmail.com>
2025-01-30 16:41:40 +0800

    lift state WeatherWidget -> Dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,9 +1,11 @@
 import TodoWidget from "./Widgets/TodoWidget";
+import WeatherWidget from "./widgets/WeatherWidget";
 
 export default function Dashboard() {
     return (
         <main>
             <h1>My Dashboard</h1>
+            <WeatherWidget />
             <TodoWidget />
         </main>
     );
