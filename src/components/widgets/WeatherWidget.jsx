5df47d81af4bff3719461d2568aca35ad7b2049a
Shawn Tan <shawnhadritan@gmail.com>
2025-01-31 15:24:52 +0800

    implement toast and other minor UI fixes

--- src/components/widgets/WeatherWidget.jsx
+++ src/components/widgets/WeatherWidget.jsx
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react";
 import { fetchWeatherData, fetchNEAWeatherData, fetchFourDayWeatherDataSG } from "../../services/service";
 import styles from './WeatherWidget.module.css'
+import { toast } from 'react-toastify';
 
 export default function WeatherWidget() {
     const [forecastData, setForecastData] = useState(null);
     const [currentWeather, setCurrentWeather] = useState(null);
-    const [countrySearch, setCountrySearch] = useState(null);
+    const [countrySearch, setCountrySearch] = useState('');
     const [townSearch, setTownSearch] = useState('');
     const [townWeather, setTownWeather] = useState('');
     const [towns, setTowns] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
         const fetchTowns = async () => {
@@ -47,17 +49,27 @@ export default function WeatherWidget() {
 
     const handleCountrySearch = async (e) => {
         e.preventDefault();
-        if (!countrySearch) return;
+        if (!countrySearch || countrySearch.trim() === '') {
+            toast.error('Enter a valid location');
+            return;
+        }
+        setIsLoading(true);
         try {
             const weather = await fetchWeatherData(countrySearch);
             setCurrentWeather({
                 temp: weather.current.temp_c,
                 condition: weather.current.condition.text,
                 icon: weather.current.condition.icon,
-                location: weather.location.name
+                country: weather.location.country,
+                city: weather.location.name
             });
+            // setCountrySearch('');
+            toast.success('Weather data fetch success')
         } catch (error) {
             console.error('fetch weather FAIL : ', error);
+            toast.error('Failed to fetch weather data. Please try again.');
+        } finally {
+            setIsLoading(false);
         }
     };
 
@@ -80,7 +92,7 @@ export default function WeatherWidget() {
     return (
         <div className={styles.weatherWidget}>
             <div className={styles.topRow}>
-                
+
                 <div className={`${styles.section} ${styles.currentWeather}`}>
                     <h3>Current Weather by Country</h3>
                     <form onSubmit={handleCountrySearch} className={styles.searchForm}>
@@ -94,9 +106,11 @@ export default function WeatherWidget() {
                         </div>
                         <button type="submit" className={styles.searchButton}><span>Search</span></button>
                     </form>
-                    {currentWeather && (
+                    {isLoading ? (
+                        <div>Loading...</div>
+                    ) : (currentWeather && (
                         <div className={styles.weatherCard}>
-                            <h4>{currentWeather.location}:</h4>
+                            <h4>{currentWeather.country}, {currentWeather.city}:</h4>
                             <div className={styles.weatherInfo}>
                                 <img src={currentWeather.icon} alt="weather-condition.jpg" />
                                 <div>
@@ -105,6 +119,7 @@ export default function WeatherWidget() {
                                 </div>
                             </div>
                         </div>
+                        )
                     )}
                 </div>
 
@@ -113,7 +128,7 @@ export default function WeatherWidget() {
                     <div className={styles.forecastGrid}>
                         {forecastData?.map((day, index) => (
                             <div key={index} className={styles.forecastItem}>
-                                <h4>{day.day} ({day.timestamp.split("T")[0]})</h4>
+                                <h4>{day.day} ({new Date(day.timestamp).toLocaleDateString('en-GB', { day: 'numeric', month: 'short' })})</h4>
                                 <p className={styles.smallText}>🔮 {day.forecast.text} ({day.forecast.summary})</p>
                                 <p className={styles.smallText}>🌡️ {day.temperature.low}°C - {day.temperature.high}°C</p>
                                 <p className={styles.smallText}>💧 {day.relativeHumidity.low}% - {day.relativeHumidity.high}%</p>
