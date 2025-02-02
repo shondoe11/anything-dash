bf51d29be742f611bd1c265b8aa17d97db7e47f3
Shawn Tan <shawnhadritan@gmail.com>
2025-02-02 10:27:00 +0800

    add FootballWidget

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,15 +1,18 @@
 import CryptoWidget from "./widgets/CryptoWidget";
 import TodoWidget from "./Widgets/TodoWidget";
 import WeatherWidget from "./widgets/WeatherWidget";
+import FootballWidget from "./widgets/FootballWidget";
 
 export default function Dashboard() {
     return (
         <main>
             <h1>My Dashboard</h1>
                 <WeatherWidget />
+                {/* temp mid row */}
                 <div style={{ display: 'flex', justifyContent: 'start'}}>
                     <CryptoWidget />
                     <TodoWidget />
+                    <FootballWidget />
                 </div>
         </main>
     );
