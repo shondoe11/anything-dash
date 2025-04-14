45165936f093da9947bffde19b17a491328e4fec
shondoe11 <shawnhadritan@gmail.com>
2025-04-14 14:50:28 +0800

    feat(ui): improved styling

--- src/App.jsx
+++ src/App.jsx
@@ -12,28 +12,42 @@ import './App.css';
 import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './theme.css';
 import HomePage from './pages/HomePage';
 import NewTodoPage from './pages/NewTodoPage';
 
 function App() {
   return (
-    <>
+    <div className="app-wrapper">
       <Navbar />
-      <Container className="py-4">
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/anything" element={<Dashboard />} />
-        <Route path="/todo" element={<TodoPage />} />
-        <Route path="/todo/new" element={<NewTodoPage />} />
-        <Route path="/todo/edit" element={<EditTodoPage />} />
-        <Route path="/weather" element={<WeatherPage />} />
-        <Route path="/crypto" element={<CryptoPage />} />
-        <Route path="/football" element={<FootballPage />} />
-        <Route path="/anime" element={<AnimePage />} />
-      </Routes>
-      </Container>
-      <ToastContainer />
-    </>
+      <main className="main-content">
+        <Container className="py-4 px-md-4 fade-in">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/anything" element={<Dashboard />} />
+            <Route path="/todo" element={<TodoPage />} />
+            <Route path="/todo/new" element={<NewTodoPage />} />
+            <Route path="/todo/edit" element={<EditTodoPage />} />
+            <Route path="/weather" element={<WeatherPage />} />
+            <Route path="/crypto" element={<CryptoPage />} />
+            <Route path="/football" element={<FootballPage />} />
+            <Route path="/anime" element={<AnimePage />} />
+          </Routes>
+        </Container>
+      </main>
+      <ToastContainer 
+        position="bottom-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+    </div>
   );
 }
 
