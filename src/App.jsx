6976ca1d7f7b0fc21fc3934a6c883743d22914ca
shondoe11 <shawnhadritan@gmail.com>
2025-04-16 10:06:41 +0800

    update imports + routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,46 +8,51 @@ import WeatherPage from './pages/WeatherPage';
 import CryptoPage from './pages/CryptoPage';
 import FootballPage from './pages/FootballPage';
 import AnimePage from './pages/AnimePage';
+import HomePage from './pages/HomePage';
+import NewTodoPage from './pages/NewTodoPage';
+import { ThemeProvider } from './context/ThemeContext';
+import { ToastContainer } from 'react-toastify';
+
+//~ CSS imports
 import './App.css';
-import {ToastContainer} from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './theme.css';
-import HomePage from './pages/HomePage';
-import NewTodoPage from './pages/NewTodoPage';
 
 function App() {
   return (
-    <div className="app-wrapper">
-      <Navbar />
-      <main className="main-content">
-        <Container className="py-4 px-md-4 fade-in">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/anything" element={<Dashboard />} />
-            <Route path="/todo" element={<TodoPage />} />
-            <Route path="/todo/new" element={<NewTodoPage />} />
-            <Route path="/todo/edit" element={<EditTodoPage />} />
-            <Route path="/weather" element={<WeatherPage />} />
-            <Route path="/crypto" element={<CryptoPage />} />
-            <Route path="/football" element={<FootballPage />} />
-            <Route path="/anime" element={<AnimePage />} />
-          </Routes>
-        </Container>
-      </main>
-      <ToastContainer 
-        position="bottom-right"
-        autoClose={3000}
-        hideProgressBar={false}
-        newestOnTop
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="colored"
-      />
-    </div>
+    <ThemeProvider>
+      <div className="app-wrapper">
+        <Navbar />
+        <main className="main-content">
+          <Container className="py-4 px-md-4 fade-in">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/anything" element={<Dashboard />} />
+              <Route path="/todo" element={<TodoPage />} />
+              <Route path="/todo/new" element={<NewTodoPage />} />
+              <Route path="/todo/edit" element={<EditTodoPage />} />
+              <Route path="/weather" element={<WeatherPage />} />
+              <Route path="/crypto" element={<CryptoPage />} />
+              <Route path="/football" element={<FootballPage />} />
+              <Route path="/anime" element={<AnimePage />} />
+            </Routes>
+          </Container>
+        </main>
+        <ToastContainer 
+          position="bottom-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
+      </div>
+    </ThemeProvider>
   );
 }
 
