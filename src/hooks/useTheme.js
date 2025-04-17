8cb8895de2c4a6e6d060a28eef2906f7d98674a4
shondoe11 <shawnhadritan@gmail.com>
2025-04-17 16:56:39 +0800

    refactor(useTheme): simplify hook API & apply CSS variable updates

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -2,6 +2,10 @@ import { useContext } from 'react';
 import ThemeContext from '../context/ThemeContext';
 
 //& custom hook to use theme context
-const useTheme = () => useContext(ThemeContext);
+const useTheme = () => {
+  const context = useContext(ThemeContext);
+  if (!context) throw new Error('useTheme must be used within ThemeProvider');
+  return context;
+};
 
 export default useTheme;
