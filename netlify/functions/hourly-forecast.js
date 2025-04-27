e43c89a25e23c9aecfaf7fa93dc47e46c273624c
shondoe11 <shawnhadritan@gmail.com>
2025-04-27 15:20:43 +0800

    feat(netlify): add 24-h forecast proxy func; CORS-friendly netlify Lambda wrapper fr NEA 24-h forecast

---
+++ netlify/functions/hourly-forecast.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef, no-unused-vars */
+//* Proxy NEA 24-hour hourly forecast: bypass CORS
+exports.handler = async function(event, context) {
+  try {
+    //~ fetch 24h forecast frm NEA
+    const res = await fetch('https://api.data.gov.sg/v1/environment/24-hour-weather-forecast');
+    const data = await res.json();
+    return {
+      statusCode: 200,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ error: error.message }),
+    };
+  }
+};
