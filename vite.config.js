041d1d1b7861eaa5cdfac722759ef91f6706efac
shondoe11 <shawnhadritan@gmail.com>
2025-05-11 10:45:00 +0800

    chore(config): proxy coingecko-proxy + football-proxy in dev

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,20 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
         rewrite: path => path.replace(/^\/api/, '')
+      },
+      //~ proxy CoinGecko thru local func
+      '/.netlify/functions/coingecko-proxy': {
+        target: 'http://localhost:8888',
+        changeOrigin: true,
+        secure: false,
+        rewrite: path => path.replace(/^\/\.netlify\/functions\/coingecko-proxy/, '')
+      },
+      //~ proxy Football req to Netlify func
+      '/.netlify/functions/football-proxy': {
+        target: 'http://localhost:8888',
+        changeOrigin: true,
+        secure: false,
+        rewrite: path => path
       }
     }
   }
